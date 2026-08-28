b5847035919c95e0170cf9f3bd82c85123d8fc12
Gary Zhang <zhanggary607@gmail.com>
2026-08-28 12:21:52 -0400

    Export context care public API

--- src/cognition_slm/__init__.py
+++ src/cognition_slm/__init__.py
@@ -1,3 +1,13 @@
-"""Inspectable mini SLM for coding behavior experiments."""
+"""Inspectable mini SLM and context-care tools for coding experiments."""
+
+from .context_therapy import ContextAssessment, ContextHandoff, ContextMessage, ContextTherapist
 
 __version__ = "0.4.0"
+
+__all__ = [
+    "ContextAssessment",
+    "ContextHandoff",
+    "ContextMessage",
+    "ContextTherapist",
+    "__version__",
+]
